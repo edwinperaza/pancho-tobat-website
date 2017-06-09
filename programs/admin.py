29ecc22603dcbcc3c483adf98e212c6e4da65aa6
Edwin Peraza <edwinperaza87@gmail.com>
2017-06-09 02:05:26 -0400

    Add talleres section and create new program

--- programs/admin.py
+++ programs/admin.py
@@ -18,7 +18,7 @@ class ProgramAdmin(SummernoteModelAdmin):
     inlines = [ImageAdminInline]
 
     def has_add_permission(self, request):
-        return False
+        return True
 
     def has_delete_permission(self, request, obj=None):
         return False
